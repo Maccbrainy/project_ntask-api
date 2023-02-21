9318d21993401b981bd2f060514d3ceb46df29c9
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-02-21 09:26:35 +0100

    Files removed; file clean up

--- libs/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const env = process.env.NODE_ENV;
-    if(env === "test") {
-      return require(`./config.test.js`);
-    } else {
-      return require("./config.development.js");
-    }
-    
-};
