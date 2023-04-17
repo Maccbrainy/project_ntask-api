8eed6284df948f6716a6c7fee50bfe16ad58ed1c
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-04-17 12:44:02 +0100

    generating app request logs

--- config/development.js
+++ config/development.js
@@ -1,3 +1,4 @@
+const logger = require("../libs/logger");
 module.exports = {
     db: {
         database: "nTask",
--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,23 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const morgan = require('morgan');
+const logger = require('./logger');
 module.exports = app => {
-    app.set("port", 5000);
+    app.set("port", 8080);
     app.set("json spaces", 4);
+    /**
+     * Log of the requests
+     * Generating request logs; to log all requests for our app
+     */
+    app.use(morgan("common", {
+        //send these logs to our module logger.js
+        stream: {
+            write: (message) => {
+                logger.info(message);
+            }
+        }
+    }))
 
     app.use(cors({
         origin: ["http://localhost:8081"],
